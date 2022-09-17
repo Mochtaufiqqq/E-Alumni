b7c7d949d2b234e22cdf16008dedd9207fad8f58
Mochtaufiqqq <mhmdtaufiq3@gmail.com>
2022-09-17 12:39:48 +0700

    lara-alumni

---
+++ resources/views/includes/admin/script.blade.php
@@ -0,0 +1,18 @@
+<!-- latest jquery-->
+<script src="viho_all/html/assets/js/jquery-3.5.1.min.js"></script>
+<!-- feather icon js-->
+<script src="viho_all/html/assets/js/icons/feather-icon/feather.min.js"></script>
+<script src="viho_all/html/assets/js/icons/feather-icon/feather-icon.js"></script>
+<!-- Sidebar jquery-->
+<script src="viho_all/html/assets/js/sidebar-menu.js"></script>
+<script src="viho_all/html/assets/js/config.js"></script>
+<!-- Bootstrap js-->
+<script src="viho_all/html/assets/js/bootstrap/popper.min.js"></script>
+<script src="viho_all/html/assets/js/bootstrap/bootstrap.min.js"></script>
+<!-- Plugins JS start-->
+<!-- Plugins JS Ends-->
+<!-- Theme js-->
+<script src="viho_all/html/assets/js/script.js"></script>
+<script src="viho_all/html/assets/js/theme-customizer/customizer.js"></script>
+<!-- login js-->
+<!-- Plugin used-->
---
+++ resources/views/layouts/admin/master.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="viho_all/html/assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="viho_all/html/assets/images/favicon.png" type="image/x-icon">
+    <title>viho - Premium Admin Template</title>
+    @include('includes.admin.style')
+    <!-- Google font-->
+  </head>
+  
+  <body>
+    <!-- Loader starts-->
+   @include('includes.admin.loader')
+    <!-- Loader ends-->
+    <!-- page-wrapper Start-->
+    <div class="page-wrapper" id="pageWrapper">
+      <!-- Page Header Start-->
+      @include('includes.admin.navbar')
+      <!-- Page Header Ends-->
+      <!-- Page Body Start-->
+      <div class="page-body-wrapper horizontal-menu">
+        <!-- Page Sidebar Start-->
+        @include('includes.admin.sidebar')
+        <!-- Page Sidebar Ends-->
+        <div class="page-body">
+          <!-- Container-fluid starts-->
+          @yield('content')
+          <!-- Container-fluid Ends-->
+        </div>
+        <!-- footer start-->
+        @include('includes.admin.footer')
+      </div>
+    </div>
+    @include('includes.admin.script')
+
+    {{-- @yield('script')
+    @yield('javascript') --}}
+  </body>
+</html>
